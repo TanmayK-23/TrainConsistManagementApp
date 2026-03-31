9c670b6334c734303b72c2d55ab0b3d889a09298
Tanmay <kumartanmay39@gmail.com>
2026-03-31 13:47:37 +0530

    UC3: Track unique bogie IDs using HashSet

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * ================================================================
@@ -111,4 +113,31 @@ class UseCase2TrainConsistMgmt {
     private static String trainConsistToString(Train train) {
         return train.toString().replace("Train Consist: ", "");
     }
+}
+
+class UseCase3TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("======================================\n");
+
+        Set<String> bogies = new HashSet<>();
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
+
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+
+        System.out.println("\nUC3 uniqueness validation completed...");    }
 }
