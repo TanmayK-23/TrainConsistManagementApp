7c12f3099fb5dcdf4f2edc4bb1e6ed19adb8d8af
Tanmay <kumartanmay39@gmail.com>
2026-03-31 13:54:48 +0530

    UC4: Maintain ordered bogie consist using LinkedList

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.LinkedList;
 
 /**
  * ================================================================
@@ -140,4 +141,50 @@ class UseCase3TrainConsistMgmt {
         System.out.println("Duplicates are automatically ignored by HashSet.");
 
         System.out.println("\nUC3 uniqueness validation completed...");    }
+}
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase4TrainConsistMgmt
+ * ================================================================
+ *
+ * Use Case 4: Maintain Ordered Bogie Consist
+ *
+ * Description:
+ * This class models the physical chaining of train bogies
+ * using LinkedList for ordered operations.
+ *
+ * @version 4.0
+ */
+class UseCase4TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("UC4 - Maintain Ordered Bogie Consist");
+        System.out.println("======================================\n");
+
+        LinkedList<String> trainConsist = new LinkedList<>();
+
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(trainConsist);
+
+        trainConsist.add(2, "Pantry Car");
+
+        System.out.println("\nAfter Inserting 'Pantry Car' at position 2:");
+        System.out.println(trainConsist);
+
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("\nAfter Removing First and Last Bogie:");
+        System.out.println(trainConsist);
+
+        System.out.println("\nUC4 ordered consist operations completed...");
+    }
 }
